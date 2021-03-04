4cbbb62de3d4c7f003e907a1f9ed65fa44872f13
Hank <hankchen1997@yahoo.com>
2021-03-04 14:13:16 -0500

    connected to DOM

---
+++ Todo/dist/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./frontend/todo_redux.jsx ***!
+  \*********************************/
+document.addEventListener("DOMContentLoaded", function () {
+  var root = document.getElementById('root');
+  var h1 = document.createElement('h1');
+  h1.textContent = "Todos App";
+  root.appendChild(h1);
+});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ Todo/frontend/todo_redux.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+    const h1 = document.createElement('h1')
+    h1.textContent = "Todos App"
+    root.appendChild(h1)
+
+})
